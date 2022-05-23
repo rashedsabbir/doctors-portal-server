4f2fc25518a221ff490c8f67226260b37adb2fb7
rashedsabbir <rashedulhasan1996@gmail.com>
2022-05-23 23:51:21 +0600

    booking added

--- index.js
+++ index.js
@@ -19,13 +19,31 @@ async function run(){
         await client.connect();
         console.log("Connected to server")
         const serviceCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('bookings');
+
         app.get('/service', async(req, res) =>{
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         })
+/**
+     * API Naming Convention
+     * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
+     * app.get('/booking/:id') // get a specific booking 
+     * app.post('/booking') // add a new booking
+     * app.patch('/booking/:id) //
+     * app.delete('/booking/:id) //
+    */
+   app.post('/booking', async(req, res)=>{
+     const booking=req.body;
+     console.log(req.body);
+     const result= await bookingCollection.insertOne(booking);
+     res.send(result);
+   })
+
     }
+  
     finally{
 
     }
