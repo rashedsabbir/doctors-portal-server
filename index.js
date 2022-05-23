59606d0c06ff674133f2903f444f47a59396f8fd
rashedsabbir <rashedulhasan1996@gmail.com>
2022-05-24 01:36:19 +0600

    my booking added

--- index.js
+++ index.js
@@ -63,6 +63,13 @@ async function run(){
      * app.patch('/booking/:id) //
      * app.delete('/booking/:id) //
     */
+ app.get('/booking', async(req, res) =>{
+  const patient = req.query.patient;
+  const query = {patient: patient};
+  const bookings = await bookingCollection.find(query).toArray();
+  res.send(bookings);
+})
+
    app.post('/booking', async(req, res)=>{
      const booking=req.body;
     //  console.log(req.body);
